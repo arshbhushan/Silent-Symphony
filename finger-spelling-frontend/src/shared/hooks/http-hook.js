4bbebd8745cb4e0fca0d43821a90fbbe79c3ea21
arshbhushan <arshbhushan@gmail.com>
2024-02-18 00:31:33 +0530

    RenderAllUsers

--- finger-spelling-frontend/src/shared/hooks/http-hook.js
+++ finger-spelling-frontend/src/shared/hooks/http-hook.js
@@ -7,48 +7,53 @@ export const useHttpClient = () => {
 
     const activeHttpRequests=useRef([]);
 
-    const sendRequest = useCallback (async(
-        url, 
-        method = 'GET', 
-        body = null, 
-        headers = {}
-        ) => {
-            setIsLoading(true);
-            const httpAbortCtrl= new AbortController();
-            activeHttpRequests.current.push(httpAbortCtrl);
+    const sendRequest = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        setIsLoading(true);
+        const httpAbortCtrl = new AbortController();
+        activeHttpRequests.current.push(httpAbortCtrl);
+    
         try {
             const response = await fetch(url, {
                 method,
                 body,
                 headers,
-                signal:httpAbortCtrl.signal
-            }); 
-            const ResponseData = await response.json();
-
-            activeHttpRequests.current=activeHttpRequests.current.filter(reqCtrl=>reqCtrl!==httpAbortCtrl);
-
-            if (!response.ok) {
-                throw new Error(ResponseData.message);
-            };
-            setIsLoading(false);
-            return ResponseData;
+                signal: httpAbortCtrl.signal
+            });
+    
+            const responseData = await response.json();
+    
+            // Check if the request was aborted
+            if (response.ok) {
+                setIsLoading(false);
+                return responseData;
+            } else {
+                throw new Error(responseData.message);
+            }
         } catch (err) {
-            setError(err.message);
-            setIsLoading(false);
-            throw err;
+            if (err.name === 'AbortError') {
+                console.log('Request aborted by user');
+                // Handle aborted request (e.g., return a specific value)
+            } else {
+                setError(err.message);
+                setIsLoading(false);
+                throw err;
+            }
         }
     }, []);
+    
     const clearError=()=>{
         setError(null);
     };
     useEffect(() => {
+        const activeHttpRequestsCopy = activeHttpRequests.current;
         return () => {
-            activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());
+            activeHttpRequestsCopy.forEach(abortCtrl => abortCtrl.abort());
         };
     }, []);
     
+    
 
 
-    return {isLoading, error, sendRequest, clearError };
+    return{isLoading, error, sendRequest, clearError};
 
 };
--- finger-spelling-frontend/src/user/pages/Auth.js
+++ finger-spelling-frontend/src/user/pages/Auth.js
@@ -136,6 +136,7 @@ const Auth = () => {
         <Button inverse onClick={switchModeHandler}>
           SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
         </Button>
+        
       </Card>
     </>
   );
