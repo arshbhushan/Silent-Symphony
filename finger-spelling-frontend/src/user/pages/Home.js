be7f1cff4d7feae1dc310d793aa624a5963a37a3
arshbhushan <arshbhushan@gmail.com>
2024-03-08 23:26:09 +0530

    changingTheHomeScreen

--- finger-spelling-frontend/src/user/pages/Home.js
+++ finger-spelling-frontend/src/user/pages/Home.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import BannerBackground from './asl_dribbble.gif'
+import BannerBackground from './home_bg_two.jpg'
 import './Home.css'
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 const Home = () => {
-
-    return (
-   <>
-      <div className='home-container' id='home'>
-        
-        <div className='home-banner-container'>
-          <img src={BannerBackground} alt='' />
-          <div className='home-text-section'>
-            <h2 className='primary-heading'>
-              learn Today,
-              Teach Tomorrow.
-            </h2>
-          </div>
+  return (
+    <div className='home-container'>
+      <div className='home-banner-container'>
+        <div className='home-text-section'>
+          <h2 className='primary-heading'>
+            Learn Today, Teach Tomorrow.
+          </h2>
+        </div>
+        <div className='home-image-section'>
+          <img src={BannerBackground} alt='' className='bg-image' />
         </div>
       </div>
-      <div>
-      </div>
-</>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Home;
+export default Home;
