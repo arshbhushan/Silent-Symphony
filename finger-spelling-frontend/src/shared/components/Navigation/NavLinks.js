735e37f336de81d4e48958741ac133a3961eb839
arshbhushan <arshbhushan@gmail.com>
2024-07-25 00:36:08 +0530

    updatedGridSection

--- finger-spelling-frontend/src/shared/components/Navigation/NavLinks.js
+++ finger-spelling-frontend/src/shared/components/Navigation/NavLinks.js
@@ -14,46 +14,38 @@ const NavLinks = () => {
   return (
     <ul className="nav-links">
       <li>
-        <NavLink to="/" exact>
-          All Users
-        </NavLink>
+        <NavLink to="/" exact>All Users</NavLink>
       </li>
-      <p className='dot-inbetween'><h2>.</h2></p>
-      {auth.isLoggedIn && (<li>
-        <NavLink to="/learnings/new">ADD LEARNINGS
 
-        </NavLink>
-      </li>)}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/learnings/new">Add Learnings</NavLink>
+        </li>
+      )}
 
       <li className="dropdown">
         <button onClick={handleDropdownClick}>Techniques</button>
         {isDropdownOpen && (
           <div className="dropdown-content">
             <NavLink to="/finger-spelling">FingerLearning</NavLink>
-            {/* <NavLink to="/finger-spelling/all-alphabets">All Alphabets</NavLink> */}
             <NavLink to="/gesture-learning">Gesture Recognition</NavLink>
           </div>
         )}
       </li>
-      <p className='dot-inbetween'><h2>.</h2></p>
-      {/* {!auth.isLoggedIn && ( <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>)} */}
+
       {auth.isLoggedIn && (
         <li>
-          <button onClick={auth.logout}>LOGOUT</button>
+          <button onClick={auth.logout}>Logout</button>
         </li>
-
       )}
-      <NavLink to="/" exact>
-        Brief
-      </NavLink>
 
-      <p className='dot-inbetween'><h2>.</h2></p>
-      <NavLink to="/" exact>
-        Features
-      </NavLink>
+      <li>
+        <NavLink to="/" exact>Brief</NavLink>
+      </li>
 
+      <li>
+        <NavLink to="/" exact>Features</NavLink>
+      </li>
     </ul>
   );
 };
