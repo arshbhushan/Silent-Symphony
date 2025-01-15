1d960ddc29abe27847ed4cf781326bb202762e2d
arshbhushan <arshbhushan@gmail.com>
2025-01-16 03:29:05 +0530

    updated user module according to roles.

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,9 +4,9 @@ const userSchema = mongoose.Schema({
     name : {type: String,required:true},
     email: {type: String,required:true,unique: true},
     password: {type: String,required:true, minlength:6},
-    image: {type: String,required:true},
+    image: {type: String,required:false},
     learnings: [{type: mongoose.Types.ObjectId,required: true, ref:'Learnings'}],
-    
+    roles: { type: [String], default: ['USER'] }
     });
     userSchema.plugin(uniqueValidator);  //This will add a unique index to the email field in MongoDB
                                         //and throw an error if we try to insert a document with duplicate email address server side
