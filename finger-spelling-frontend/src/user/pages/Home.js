ba2865efc2b5a333c9f8e893a3debe326c5b151b
arshbhushan <arshbhushan@gmail.com>
2025-01-18 03:24:40 +0530

    hides the login/signup button post login.

--- finger-spelling-frontend/src/user/pages/Home.js
+++ finger-spelling-frontend/src/user/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 import Brief from '../../fingerSpelling/components/Brief.js';
 // Your component code here
@@ -9,8 +9,11 @@ import Button from '../../shared/components/FormElements/Button.js'
 // import HorizontalScroller from '../../fingerSpelling/components/HorizontalScroller.js';
 // import About from '../../fingerSpelling/components/AboutUs.js';
 import './Home.css'
+import { AuthContext } from '../../shared/context/auth-context.js';
 
 const Home = () => {
+  const auth = useContext(AuthContext); // Access AuthContext
+
   return (
     <>
       <div className='home-container'>
@@ -19,40 +22,35 @@ const Home = () => {
             <h2 className='primary-heading'>
               <b>Silent Symphony</b> 
             </h2>
-            <br></br>
+            <br />
             <p className='sub-text'>
-            Silent Symphony: Your Sign Language Companion. Learn to communicate effortlessly with the deaf and mute community. 
-            Dive into our intuitive lessons and empower yourself to connect with others through the universal language of signs.</p>
-            <br></br>
-            <br></br>
-            <div className='buttons'>
-              <Link to="/auth">
-                <Button>LOGIN</Button>
-              </Link>
-              <Link to="/auth">
-                <Button>SIGNUP</Button>
-              </Link>
-            </div>
+              Silent Symphony: Your Sign Language Companion. Learn to communicate effortlessly with the deaf and mute community. 
+              Dive into our intuitive lessons and empower yourself to connect with others through the universal language of signs.
+            </p>
+            <br />
+            <br />
+            {!auth.isLoggedIn && ( // Conditionally render buttons based on login status
+              <div className='buttons'>
+                <Link to="/auth">
+                  <Button>LOGIN</Button>
+                </Link>
+                <Link to="/auth">
+                  <Button>SIGNUP</Button>
+                </Link>
+              </div>
+            )}
           </div>
           <div className='home-image-section'>
-            {/* <img src={BannerBackground} alt='' className='bg-image' /> */}
             <br />
           </div>
-
         </div>
-
-
       </div>
       <div>
-      <Brief />
+        <Brief />
       </div>
-      {/* <div><HorizontalScroller/></div>
-    <div><About/></div> */}
-      {/* <div className='modelTD'>
-
-      </div> */}
     </>
   );
 };
+
 
 export default Home;
