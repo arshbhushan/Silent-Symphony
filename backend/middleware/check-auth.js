6cb9f3c0044dec1227be56fe17c49c347773a460
arshbhushan <arshbhushan@gmail.com>
2025-02-13 13:40:07 +0530

    roles  added while auth checking.

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import {HttpError} from '../models/http-error.js'
+import { HttpError } from '../models/http-error.js';
 
 const checkAuth = (req, res, next) => {
   if (req.method === 'OPTIONS') {
@@ -11,7 +11,7 @@ const checkAuth = (req, res, next) => {
       throw new Error('Authentication failed!');
     }
     const decodedToken = jwt.verify(token, 'supersecret_dont_share');
-    req.userData = { userId: decodedToken.userId };
+    req.userData = { userId: decodedToken.userId, roles: decodedToken.roles }; // Attach roles
     next();
   } catch (err) {
     const error = new HttpError('Authentication failed!', 401);
