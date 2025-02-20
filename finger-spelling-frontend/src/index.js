71c0ffd13b1c8144f080b56546550d9d6892c052
arshbhushan <arshbhushan@gmail.com>
2025-02-20 21:40:29 +0530

    removed unwanted comments.

--- finger-spelling-frontend/src/index.js
+++ finger-spelling-frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Importing ReactDOM from 'react-dom/client' for concurrent mode
-import './index.css'; // Importing styles
-import App from './App'; // Importing the main App component
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Creating a root using ReactDOM.createRoot (concurrent mode)
+// Creating a root.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Rendering the App component within a React.StrictMode for additional checks during development
 root.render(
   <React.StrictMode>
     <App />
@@ -15,5 +14,4 @@ root.render(
 );
 
 
-// If you want to measure performance, you can use reportWebVitals
 reportWebVitals();
