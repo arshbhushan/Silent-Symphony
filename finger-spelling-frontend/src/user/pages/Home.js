a4183229fd4b6f802da43c1128b80a859b621831
arshbhushan <arshbhushan@gmail.com>
2025-03-05 15:50:14 +0530

    added image in home component.

--- finger-spelling-frontend/src/user/pages/Home.js
+++ finger-spelling-frontend/src/user/pages/Home.js
@@ -1,14 +1,13 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Brief from '../../fingerSpelling/components/Brief.js';
-
-//import BannerBackground from './image.png';
-import Button from '../../shared/components/FormElements/Button.js'
-import './Home.css'
+import BannerBackground from '../pages/home_bg_two.jpg'; // Import the image
+import Button from '../../shared/components/FormElements/Button.js';
+import './Home.css';
 import { AuthContext } from '../../shared/context/auth-context.js';
 
 const Home = () => {
-  const auth = useContext(AuthContext); 
+  const auth = useContext(AuthContext);
 
   return (
     <>
@@ -16,11 +15,11 @@ const Home = () => {
         <div className='home-banner-container'>
           <div className='home-text-section'>
             <h2 className='primary-heading'>
-              <b>Silent Symphony</b> 
+              <b>Silent Symphony</b>
             </h2>
             <br />
             <p className='sub-text'>
-              Silent Symphony: Your Sign Language Companion. Learn to communicate effortlessly with the deaf and mute community. 
+              Silent Symphony: Your Sign Language Companion. Learn to communicate effortlessly with the deaf and mute community.
               Dive into our intuitive lessons and empower yourself to connect with others through the universal language of signs.
             </p>
             <br />
@@ -37,7 +36,7 @@ const Home = () => {
             )}
           </div>
           <div className='home-image-section'>
-            <br />
+            <img src={BannerBackground} alt="Banner" className='bg-image' />
           </div>
         </div>
       </div>
@@ -47,6 +46,5 @@ const Home = () => {
     </>
   );
 };
-
 
 export default Home;
